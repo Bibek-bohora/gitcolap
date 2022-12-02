fe829eb656a8333365338b7f9308b232d08d9ed1
Bibek Bohora <85892685+Bibek-bohora@users.noreply.github.com>
2022-12-02 13:09:13 +0545

    wer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,11 @@ const About = () => {
   return (
     <div>
        <h1>hello this is from about page hee</h1>
+       <p>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint distinctio natus aperiam in reprehenderit, assumenda necessitatibus hic incidunt ex unde asperiores architecto provident sapiente sunt sequi omnis magnam, voluptas praesentium.</p>
+       <p>
+       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quaerat accusantium adipisci sunt quae, harum, dicta ut enim sed inventore sapiente voluptas consequatur? Voluptas, doloribus quidem possimus distinctio a sapiente.11
+       </p>
     </div>
   )
 }
