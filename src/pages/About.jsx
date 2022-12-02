74efd4824db4a549368ccd76045eb0be10ac1d15
Bibek Bohora <85892685+Bibek-bohora@users.noreply.github.com>
2022-12-02 13:11:14 +0545

    helloo about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div>
        <h1>hello this is from about page hee</h1>
+       <h1>this about change from bibek chhetri</h1>
        <p>
        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint distinctio natus aperiam in reprehenderit, assumenda necessitatibus hic incidunt ex unde asperiores architecto provident sapiente sunt sequi omnis magnam, voluptas praesentium.</p>
        <p>
