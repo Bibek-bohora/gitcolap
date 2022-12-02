ada014794d2a581af66c71322da59c234afcfee7
Asim-sharma <80588327+AsimSharma@users.noreply.github.com>
2022-12-02 13:02:41 +0545

    change in home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-
-    <h1>this is from home pages</h1>
+         <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum nisi quasi excepturi! Voluptatibus dicta delectus suscipit nam ipsam ratione! Repellendus voluptates consequatur laborum. Nam, asperiores ea? Praesentium ea beatae omnis.</h1>
+     <a href="/">Home</a>
+     <a href="/about">About</a>
       
     </div>
   )
