a658aac86233d528296ce6f03b1f6be22c682ff2
Bibek Bohora <85892685+Bibek-bohora@users.noreply.github.com>
2022-12-02 13:42:21 +0545

    about change
    asdasdasd

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,13 +3,8 @@ import React from 'react'
 const About = () => {
   return (
     <div>
-       <h1>hello this is from about page hee</h1>
-       <h1>this about change from bibek chhetri</h1>
-       <p>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint distinctio natus aperiam in reprehenderit, assumenda necessitatibus hic incidunt ex unde asperiores architecto provident sapiente sunt sequi omnis magnam, voluptas praesentium.</p>
-       <p>
-       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quaerat accusantium adipisci sunt quae, harum, dicta ut enim sed inventore sapiente voluptas consequatur? Voluptas, doloribus quidem possimus distinctio a sapiente.11
-       </p>
+      
+      <h1>this is about pages</h1>
     </div>
   )
 }
