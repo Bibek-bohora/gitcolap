1e3e92fcd6e7b4a2add7643fe8398d2be5b2e8a9
Asim-sharma <80588327+AsimSharma@users.noreply.github.com>
2022-12-02 13:16:15 +0545

    login change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ const Home = () => {
          <h1>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorum nisi quasi excepturi! Voluptatibus dicta delectus suscipit nam ipsam ratione! Repellendus voluptates consequatur laborum. Nam, asperiores ea? Praesentium ea beatae omnis.</h1>
      <a href="/">Home</a>
      <a href="/about">About</a>
-      
+    <a href="login">Login</a> 
     </div>
   )
 }
